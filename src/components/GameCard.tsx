4168aa046434dd63c650f8fc66ccea047f2b3180
Slienfore <Slienfore@163.com>
2024-05-09 19:36:59 +0800

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import type { Game } from "../hooks/useGames";
+import PlatformIconList from "./platformIconList";
 
 interface Props {
   game: Game;
@@ -15,6 +16,9 @@ const GameCard = ({ game }: Props) => {
         {/* 标题 */}
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((item) => item.platform)} // 将每一个对象中的 platform 收集起来
+          ></PlatformIconList>
         </CardBody>
       </Card>
     </>
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,47 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // [key: string]: TS 中的索引签名语法, 定义一个对象的属性键值对的类型
+  // 键是字符串，对应的值是不同的图标组件
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <>
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
