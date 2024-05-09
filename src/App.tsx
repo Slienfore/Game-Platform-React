7620827df91406f6c8355df2758027e582c50561
Slienfore <Slienfore@163.com>
2024-05-09 10:12:25 +0800

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        // mobile device
+        base: `"nav" "main"`,
+        // large device
+        lg: `"nav nav"
+             "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      {/* 适配设备:大屏幕才展示 aside, 小屏幕隐藏
+                  above is similar to "min-width"
+                  below is similar to "max-width"
+       */}
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
